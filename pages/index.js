751c7bab319b6c95e522200636c4c9ef8946b5b0
Kraken <highlyflammable12@gmail.com>
2022-11-20 13:26:19 +0530

    index fix

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React from 'react'
 import styles from '../styles/Home.module.css'
-import Purchase from './purchase/index'
+import Purchase from './Purchase/index'
 
 export default function Home() {
   const router = useRouter()
